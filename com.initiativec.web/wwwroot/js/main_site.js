8f20b371f34e47b5b3a5fac5537bf19eabd7b2cc
Raphael Santo <raphael.esanto@gmail.com>
2024-07-30 01:24:38 -0300

    Arrumando menu Saiba Mais

--- com.initiativec.web/wwwroot/js/main_site.js
+++ com.initiativec.web/wwwroot/js/main_site.js
@@ -1,4 +1,103 @@
 // A $( document ).ready() block.
 $(document).ready(function () {
-    console.log("ready!");
+    
+
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        
+        if (scroll > 50) {
+            
+            $('.NavBar.sticky').addClass('active');
+        } else {
+            $('.NavBar.sticky').removeClass('active');
+        }
+
+
+
+        //Alterando o cor do logo
+        var scrollTop = $(this).scrollTop();
+        var documentHeight = $(document).height();
+        var windowHeight = $(window).height();
+
+        var scrollPercent = (scrollTop / (documentHeight - windowHeight)) * 100;
+
+        var $logo = $('.Logo');
+
+        // Remover todas as classes de estado do logo
+        $logo.removeClass('green golden turquoise ');
+
+        // Adicionar classe baseada na posição de rolagem
+        if (scrollPercent >= 100) {
+            $logo.addClass('green');
+        } else if (scrollPercent >= 50) {
+            $logo.addClass('golden');
+        } else if (scrollPercent >= 10) {
+            $logo.addClass('turquoise');
+        }
+    });
+
+    // Função para deslizar suavemente para a seção
+    $('a[href*="#"]').on('click', function (e) {
+        e.preventDefault();
+
+        var target = $(this.hash);
+        if (target.length) {
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 500, 'linear'); // Ajuste a duração conforme necessário
+        }
+    });
+
+
+    //Funcao para tooltip
+    $('.NavBar-link-wrapper.js-tooltip').on('click', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        var toolTip = $(this).attr('aria-describedby');
+
+
+        setTimeout(() => {
+            $('#' + toolTip).attr('aria-hidden', 'false');
+            $('#' + toolTip).css({
+                'z-index': '9999',
+                'visibility': 'visible',
+                'transition-duration': '350ms',
+                'position': 'absolute',
+                'transform': 'translate3d(333px, 63px, 0px)',
+                'top': '0px',
+                'left': '0px',
+                'will-change': 'transform'
+            });
+        }, 300);
+
+        // Clique fora do tooltip para escondê-lo
+        $(document).on('click.hideTooltip', function (event) {
+            if (!$(event.target).closest('#' + toolTip).length) {
+
+                setTimeout(() => {
+                    $('#' + toolTip).attr('aria-hidden', 'false');
+                    $('#' + toolTip).css({
+                        'z-index': '9999',
+                        'visibility': 'hidden',
+                        'transition-duration': '350ms',
+                        'position': 'absolute',
+                        'transform': 'translate3d(333px, 63px, 0px)',
+                        'top': '0px',
+                        'left': '0px',
+                        'will-change': 'transform'
+                    });
+                }, 300);
+
+                $(document).off('click.hideTooltip'); // Remove o evento para evitar múltiplas ligações
+            }
+        });
+    });
+
+    // Impede que o clique dentro do tooltip feche ele
+    $(document).on('click', '[id^="tippy-tooltip-"]', function (e) {
+        e.stopPropagation();
+    });
+
+
 });
