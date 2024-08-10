67d666f79053e78d434279206ce3bb798e832d66
santoraphael <raphael.esanto@gmail.com>
2024-08-10 16:25:58 -0300

    asd

--- com.initiativec.webpages/Pages/Index.cshtml.cs
+++ com.initiativec.webpages/Pages/Index.cshtml.cs
@@ -12,9 +12,53 @@ namespace com.initiativec.webpages.Pages
             _logger = logger;
         }
 
+        public NumberModel NumberData { get; set; }
         public void OnGet()
         {
-
+            long number = 2510483431;
+            NumberData = ProcessNumber(number);
         }
+
+        private NumberModel ProcessNumber(long number)
+        {
+            var numberString = number.ToString("N0").Replace(".", ","); // Formata o número com vírgulas
+            var digits = numberString.Replace(",", "").Select(digit => int.Parse(digit.ToString())).ToList();
+
+            var commaPositions = numberString.Select((c, i) => new { c, i })
+                                             .Where(x => x.c == ',')
+                                             .Select(x => x.i - x.i / 4)
+                                             .ToList();
+
+            var numberModel = new NumberModel();
+
+            foreach (var digit in digits)
+            {
+                numberModel.Cards.Add(new CardModel
+                {
+                    UpperValue = digit,
+                    LowerValue = 0, // Ajuste conforme necessário
+                    FirstFlipValue = 0, // Ajuste conforme necessário
+                    SecondFlipValue = digit
+                });
+            }
+
+            numberModel.CommaPositions = commaPositions;
+
+            return numberModel;
+        }
+    }
+
+    public class NumberModel
+    {
+        public List<CardModel> Cards { get; set; } = new List<CardModel>();
+        public List<int> CommaPositions { get; set; } = new List<int>();
+    }
+
+    public class CardModel
+    {
+        public int UpperValue { get; set; }
+        public int LowerValue { get; set; }
+        public int FirstFlipValue { get; set; }
+        public int SecondFlipValue { get; set; }
     }
 }
