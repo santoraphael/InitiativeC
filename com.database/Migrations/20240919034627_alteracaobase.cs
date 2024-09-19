ed8b25fe6d0908bf48e1b3d4913870763e2e2bd4
santoraphael <raphael.esanto@gmail.com>
2024-09-19 01:06:49 -0300

    swerwer

---
+++ com.database/Migrations/20240919034627_alteracaobase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace com.database.Migrations
+{
+    /// <inheritdoc />
+    public partial class alteracaobase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- com.initiativec.webpages/Pages/account/Index.cshtml.cs
+++ com.initiativec.webpages/Pages/account/Index.cshtml.cs
@@ -1,27 +1,87 @@
+using Blockfrost.Api;
+using com.cardano;
+using com.database;
+using com.database.entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace com.initiativec.webpages.Pages.account
 {
     public class IndexModel : PageModel
     {
+        private readonly DatabaseContext _context;
+        private readonly BlockfrostServices _blockfrostServices;
+
+        public IndexModel(DatabaseContext context, BlockfrostServices blockfrostServices)
+        {
+            _context = context;
+            _blockfrostServices = blockfrostServices;
+        }
+
         [BindProperty(SupportsGet = true)]
         public string InviteCode { get; set; }
 
-        public void OnGet()
+        [BindProperty]
+        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
+        public string Name { get; set; }
+
+        [BindProperty]
+        public string WalletAddress { get; set; }
+
+        public async Task OnGetAsync()
         {
-            // Utilize InviteCode conforme necessário
+            var dasd = InviteCode;
+            //Users = await _context.Users.ToListAsync();
         }
 
-        public async Task<IActionResult> OnPostAsync()
+        public IActionResult OnPost()
         {
-            // Processo de registro
-            // Associe o código de convite ao novo usuário, se necessário
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
 
-            // Marque o código como usado, se aplicável
-            // await MarkInviteCodeAsUsed(InviteCode);
+            if (string.IsNullOrEmpty(WalletAddress))
+            {
+                ModelState.AddModelError(string.Empty, "Endereço da carteira não encontrado.");
+                return Page();
+            }
 
-            return RedirectToPage("/Index");
+            var StakeAddress = _blockfrostServices.GetStakeAddress(WalletAddress);
+            var stk_adress = "";
+
+            if (StakeAddress != null)
+            {
+                stk_adress = StakeAddress.Result;
+            }
+            else
+            {
+                stk_adress = WalletAddress;
+            }
+
+
+            User user = new User()
+            {
+                wallet_address = stk_adress,
+                name = Name,
+                email = "", // Ou null, se permitido
+                phone_number = "", // Ou null
+                invite_code = "", // Ou null
+                invited_by = "", // Ou null
+                status = 0, // Ou outro valor padrão, ou null se permitido
+                confirmation_code_number = "", //GenerateConfirmationNumber(),
+                confirmation_code_alphanumber = "", // GenerateConfirmationCode(),
+                confirmed = false,
+                invitations_available = 0, // Ou outro valor padrão
+                expiration_date_invitations = DateTime.UtcNow.AddDays(7) // Ou DateTime.UtcNow.AddDays(7), por exemplo
+            };
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+
+            return RedirectToPage("/dashboard");
         }
 
         // Método opcional para marcar o código como usado
