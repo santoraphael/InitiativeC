ec6ec18945fcdf4290b6ebb8be06a4966876c7bd
Raphael Santo <raphael.esanto@gmail.com>
2024-10-23 14:45:41 -0300

    asd

--- com.initiativec.webpages/Program.cs
+++ com.initiativec.webpages/Program.cs
@@ -15,8 +15,51 @@ using Microsoft.Extensions.Options;
 using System.Security.Claims;
 using Telegram.Bot;
 using System.Text.Json;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VaultSharp;
+using VaultSharp.V1;
+using VaultSharp.V1.AuthMethods;
+using VaultSharp.V1.SecretsEngines;
+using VaultSharp.V1.AuthMethods.Token;
+using VaultSharp.V1.SystemBackend;
+using VaultSharp.Core;
+using VaultSharp.V1.Commons;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+
+// Configuração do Vault
+var vaultUrl = "http://cloud.weela.co:8200/"; // Substitua pela URL do seu Vault
+var token = "hvs.IAdrk1fNxKTAaKT4UrkPHvlQ"; // Substitua pelo seu token de autenticação
+
+// Configuração do cliente do Vault
+IAuthMethodInfo authMethod = new TokenAuthMethodInfo(token);
+var vaultClientSettings = new VaultClientSettings(vaultUrl, authMethod);
+IVaultClient vaultClient = new VaultClient(vaultClientSettings);
+
+var healthCheck = await vaultClient.V1.System.GetHealthStatusAsync();
+
+// Lendo segredos do Vault
+// Lendo segredos do Vault (KV v2)
+var mountPoint = "secret"; // Caminho onde o KV está montado
+var secretPath = "appsettings"; // Nome do seu segredo
+
+try
+{
+    var secret = await vaultClient.V1.Secrets.KeyValue.V2.ReadSecretAsync(path: secretPath, mountPoint: mountPoint);
+    // Adiciona as configurações do Vault ao Configuration
+    foreach (var entry in secret.Data.Data)
+    {
+        builder.Configuration[entry.Key] = entry.Value?.ToString();
+    }
+}
+catch (VaultApiException ex)
+{
+    var teste = ex;
+}
+
 
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
@@ -25,17 +68,19 @@ builder.Services.AddRazorPages();
 
 builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
 
-builder.Services.AddDbContext<DatabaseContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+//builder.Services.Configure<EmailApiSettings>(builder.Configuration.GetSection("EmailApiSettings"));
+//builder.Services.AddHttpClient<EmailService>();
+
+
+builder.Services.AddDbContext<DatabaseContext>(options => options.UseNpgsql(builder.Configuration["ConnectionStrings"]));
 builder.Services.Configure<TwitterSettings>(builder.Configuration.GetSection("Twitter"));
 
 // Registrar as configurações do Telegram Bot
-builder.Services.Configure<TelegramBotSettings>(builder.Configuration.GetSection("TelegramBot"));
 
 // Registrar o cliente do Telegram.Bot
 builder.Services.AddSingleton<ITelegramBotClient>(sp =>
 {
-    var settings = sp.GetRequiredService<IOptions<TelegramBotSettings>>().Value;
-    return new TelegramBotClient(settings.Token);
+    return new TelegramBotClient(builder.Configuration["TelegramBot/Token"]);
 });
 
 builder.Services.Configure<RequestLocalizationOptions>(
@@ -58,8 +103,8 @@ builder.Services.AddAuthentication(options =>
 .AddCookie()
 .AddOAuth("Discord", options =>
 {
-    options.ClientId = builder.Configuration["Discord:ClientId"];
-    options.ClientSecret = builder.Configuration["Discord:ClientSecret"];
+    options.ClientId = "1288955489224233124";//builder.Configuration["Discord:ClientId"];
+    options.ClientSecret = "gcYaZV93s8cpPRJQ-_TZfTkTOJs5y4fo"; //builder.Configuration["Discord:ClientSecret"];
     options.CallbackPath = new PathString("/signin-discord"); // Deve corresponder à Redirect URI registrada
 
     options.AuthorizationEndpoint = "https://discord.com/api/oauth2/authorize";
@@ -108,8 +153,8 @@ builder.Services.AddMvc().AddViewLocalization().AddDataAnnotationsLocalization()
 // builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddSingleton<SharedResourceService>();
 
-var apiKey = builder.Configuration["Blockfrost:Network:ApiKey"];
-var baseUrl = builder.Configuration["Blockfrost:Network:BaseUrl"];
+var apiKey = builder.Configuration["Blockfrost/Network/ApiKey"];
+var baseUrl = builder.Configuration["Blockfrost/Network/BaseUrl"];
 var authConfig = new AuthHeaderConfiguration(apiKey, baseUrl);
 
 builder.Services.AddBlockfrost(authConfig);
